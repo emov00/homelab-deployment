8eb7373e9eb744782277f1c850c2a3579f208ffb
Eli Movitz <emov7485@gmail.com>
2026-07-06 21:31:19 -0500

    finals

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # homelab-deployment
+
+
+https://argo-cd.readthedocs.io/en/stable/user-guide/app_deletion/#app-deletion
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,16 @@
 kubectl apply -f homelab/projects/bootstrap-project-project.yaml 
 
 kubectl apply -f homelab/cluster/homelab-cluster.yaml 
+
+kubectl apply -f bootstrap.yaml
+
+sync bootstrap
+
+sync bootstrap-cluster
+
+sync bootstrap-projects
+
+sync bootstrap-infrastructure
+
+sync bootstrap-apps
+
